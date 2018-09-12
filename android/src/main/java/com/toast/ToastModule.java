06d88e19a19519adfd8f370a119036dd2bc231fd
Shivam <shivam160291@gmail.com>
2018-09-12 23:06:37 +0530

    initial commit

---
+++ android/src/main/java/com/toast/ToastModule.java
@@ -0,0 +1,25 @@
+package com.toast;
+ 
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import android.widget.Toast;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+  
+  public ToastModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+ 
+  @Override
+  public String getName() {
+    return "ToastModule";
+  }
+ 
+  @ReactMethod
+  public void show(string message) {
+    Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+  }
+}
